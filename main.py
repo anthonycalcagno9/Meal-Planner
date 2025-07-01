f41f393f3ccb86a2161aae93b6cf51dbe301dedf
anthonycalcagno9 <acalcagno9@gmail.com>
2025-06-30 20:33:40 -0500

    added cors settings

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api import health, generateMealPlan
 
 app = FastAPI(
@@ -10,6 +11,15 @@ app = FastAPI(
 # Include routers
 app.include_router(health.router)
 app.include_router(generateMealPlan.router)
+
+# Configure CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
